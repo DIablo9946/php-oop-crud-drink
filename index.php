f188e33d0a71bc597e34946d89a2c3d02cbb2d67
Diablo9946 <diabloch9946@gmail.com>
2019-10-30 15:00:22 +0100

    Done.

--- index.php
+++ index.php
@@ -23,5 +23,20 @@
     echo $drink . "<br>";
   };
   echo "<br>-------------------<br><br>";
+  $min = $drinks[0];
+  $max = $drinks[0];
+  $sum = 0;
+  foreach ($drinks as $drink) {
+    if ($min -> getPrice() > $drink -> getPrice()){
+      $min = $drink;
+    }
+    if ($max -> getPrice() < $drink -> getPrice()){
+      $max = $drink;
+    }
+    $sum += $drink -> getPrice();
+  }
+  echo "MIN: " . $min . "<br>"
+        . "MAX: " . $max . "<br>"
+        . "SUM: " . $sum;
 
 ?>
